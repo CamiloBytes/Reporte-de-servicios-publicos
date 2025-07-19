46b36d12342f2f0d083601ee34f944920ac393c3
camilo Parra <camiloandres02222@gmail.com>
2025-07-18 20:06:17 -0500

    fix: vamo a corregir

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// Esta función la hacemos global para que el HTML pueda usarla con onclick=""
+window.irAlLogin = function () {
+    // Redirige a la vista de login
+    window.location.href = "/src/views/login.html";
+  };
