b513cf5651cfa0e982ee4cfc08b622db3480e532
camilo Parra <camiloandres02222@gmail.com>
2025-07-18 20:27:22 -0500

    feat:We finished connecting the styles in the project and the routes through the buttons.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 // Esta función la hacemos global para que el HTML pueda usarla con onclick=""
-window.irAlLogin = function () {
+window.goToLogin = function () {
     // Redirige a la vista de login
     window.location.href = "/src/views/login.html";
-  };
+}
+// Esta función la hacemos global para que el HTML pueda usarla con onclick=""
+window.goToHome = function () {
+    // Redirige a la vista de home
+    window.location.href = "../../index.html";
+}
+// Esta función la hacemos global para que el HTML pueda usarla con onclick=""
+window.goToForm = function () {
+    // Redirige a la vista del formulario
+    window.location.href = "/src/views/form.html";
+}
