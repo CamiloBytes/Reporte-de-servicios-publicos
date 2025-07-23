e5e93833e50a29a7597099b6a675c40237495256
JonathanLop1 <147114171+JonathanLop1@users.noreply.github.com>
2025-07-23 18:12:44 -0500

    Add files via upload
    Fix1.0

--- src/js/auth.js
+++ src/js/auth.js
@@ -2,9 +2,8 @@ export function authGuard(path) {
     const user = JSON.parse(localStorage.getItem("user"));
 
     if (!user && path === "/dashboard") {
-        return "/login"; // si no hay usuario, lo redirige al login
+        return "/login"; // redirect to login if no user found
     }
 
-    return path; // si todo bien, sigue la ruta
+    return path; // proceed to the requested path
 }
-
